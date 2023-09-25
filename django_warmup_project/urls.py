9ea4571b442d1aa3dfe4e8bada2eb171c7ad9156
mrsamsonn <andrewsj.ahs@gmail.com>
2023-09-24 21:02:04 -0700

    users app > create register form

--- django_warmup_project/urls.py
+++ django_warmup_project/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from users import views as user_views
+
 urlpatterns = [
+    path('register/', user_views.register, name='register'),
     path('', include('homepage.urls'), name='homepage'),
     path('admin/', admin.site.urls),
 ]
